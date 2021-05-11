91fb47eb25acca589c704676daa99518356771be
Sociosarbis <136657577@qq.com>
2021-05-12 01:01:00 +0800

    feat: :sparkles: 区分环境

--- functions/ssr.ts
+++ functions/ssr.ts
@@ -5,8 +5,15 @@ import { createApp } from './ssr/main';
 
 async function handler() {
   const output = createApp();
-  const template: string = (await axios.get('http://127.0.0.1:8888/index.html'))
-    .data;
+  const template: string = (
+    await axios.get(
+      `${
+        process.env.CONTEXT === 'dev'
+          ? 'http://127.0.0.1:8888'
+          : 'https://sprite-generator.netlify.app'
+      }/index.html`,
+    )
+  ).data;
   const html = renderToString(output.app);
   return new Response(
     200,
